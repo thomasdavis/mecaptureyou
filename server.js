1b1a1c16db07111777c62d9e17ed6ad9f5a7977d
Thomas Davis <thomasalwyndavis@gmail.com>
2017-08-09 00:02:35 +1000

    Ba

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const next = require('next')
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const handler = routes.getRequestHandler(app)
+const next = require('next');
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
 
 // With express.js
-const express = require('express')
-app.prepare().then(() => {
-  express().use(handler).listen(process.env.PORT || 3000)
-})
+const express = require('express');
+app.prepare().then(() => {});
