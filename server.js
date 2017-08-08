a076c9f9c5514204a42035d96dace4c6525340d6
Thomas Davis <thomasalwyndavis@gmail.com>
2017-08-09 00:09:12 +1000

    Ba

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const app = next({ dev: process.env.NODE_ENV !== 'production' });
 
 // With express.js
 const express = require('express');
-app.prepare().then(() => {});
+app.prepare().then(() => {
+  express().listen(process.env.PORT || 3000);
+});
